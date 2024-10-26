a7cc4bece204dd02fac6d33e3437ac362aaeb6fa
Ken-2511 <iwmain@outlook.com>
2024-10-26 23:46:32 +0100

    integrate openai api in it & set up proxy for testing

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+import os
 
 
 class ChatMessage(BaseModel):
@@ -7,6 +10,25 @@ class ChatMessage(BaseModel):
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+api_key = os.getenv("OPENAI_API_KEY")
+print(api_key)
+client = OpenAI(api_key=api_key)
+
+def get_response(message):
+    response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": message}],
+    )
+    return str(response.choices[0].message.content)
+
 
 @app.post("/echo")
 async def echo(message: str):
@@ -18,4 +40,7 @@ async def hello():
 
 @app.post("/api/chat")
 async def chat(cm: ChatMessage):
-    return {"response": cm.message}
+    print("Message: ", cm.message)
+    response = get_response(cm.message)
+    print(response)
+    return {"response": response}
